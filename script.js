99d7ba5051b6ba092cbb2ebdea1f2e228588827b
Jamil Mariano <marianojamil15@gmail.com>
2024-09-27 12:35:39 +0800

    Added Random Cats Generator

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* ---------------------------------Add Box Button------------------------------------- */
 // Select Button and the Container
 let addBoxButton = document.getElementById("addBoxButton");
 let container = document.querySelector(".container");
@@ -29,3 +30,34 @@ addBoxButton.addEventListener("click", function(){
     // Append the new box to the container
     container.appendChild(newBox);
 })
+
+
+/* ---------------------------------Random Cat------------------------------------- */
+
+function randomPosition(max){
+    return Math.floor(Math.random() * max);
+}
+
+function addRandomCatIcons(numIcons){
+    let body = document.body;
+
+    for(let i = 0; i < numIcons; i++){
+        let catIcon = document.createElement('img');
+        catIcon.src = './images/cat.png';
+        catIcon.classList.add('cat-icon');
+
+        const randomX = randomPosition(window.innerWidth - 50);
+        const randomY =randomPosition(window.innerHeight - 50);
+    
+        catIcon.style.width = "50px";
+        catIcon.style.height = "50px";
+        catIcon.style.position = "absolute";
+        catIcon.style.left = `${randomX}px`;
+        catIcon.style.top = `${randomY}px`
+    
+        body.appendChild(catIcon);
+    }
+}
+
+addRandomCatIcons(20);
+
