fee805303ec0b07fb0b22da84f2725ccbfc84d5c
Jamil Mariano <marianojamil15@gmail.com>
2024-10-02 15:33:22 +0800

    Added Tetrominoes

--- Tetris/script.js
+++ Tetris/script.js
@@ -14,8 +14,34 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
 
     const zTetronimo = [
-        [width, width+1, 1, 2]
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1]
     ]
+
+    const tTetromino = [
+        [1, width, width+1, width+2],
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
+
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 })
 
 
